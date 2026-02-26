24f6d23009e7d98aa011ac15537a352bf37d3963
SardarHossaini <sardarhossaini2022@gmail.com>
2026-02-26 20:40:57 +0430

    feat: design the forgot password page

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,10 +1,72 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
+class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
 
   @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  bool clrButton = false;
+  TextEditingController emailController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("forgot password page"));
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent, foregroundColor: Colors.black),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Text("Forgot password", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              SizedBox(height: 40),
+              Text(
+                "Please enter you email address. You will receive a link to create or set a new password via email.",
+                style: TextStyle(fontSize: 15),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: emailController,
+                onChanged: (val) {
+                  if (val != "") {
+                    setState(() {
+                      clrButton = true;
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                  suffix: InkWell(
+                    onTap: () {
+                      setState(() {
+                        emailController.clear();
+                      });
+                    },
+                    child: Icon(CupertinoIcons.multiply, color: Color(0xFFDB3022)),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size.fromHeight(55),
+                  backgroundColor: Color(0xFFDB3022),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                ),
+                child: Text("Send code", style: TextStyle(color: Colors.white, fontSize: 18)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
