c9083e563eac7de92f0faef382f69b87cfa55629
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-01 23:50:06 +0430

    feat: replace BottomNavigationBar with AnimatedBottomNavigationBar for improved UI

--- lib/screens/navigation_screen.dart
+++ lib/screens/navigation_screen.dart
@@ -1,7 +1,9 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:e_commerce_app/screens/cart_screen.dart';
 import 'package:e_commerce_app/screens/favorites_screen.dart';
 import 'package:e_commerce_app/screens/home_screen.dart';
 import 'package:e_commerce_app/screens/profile_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NavigationScreen extends StatefulWidget {
@@ -21,24 +23,27 @@ class _NavigationScreenState extends State<NavigationScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Navigation Screen')),
       body: IndexedStack(index: pageIndex, children: pages),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.lightBlueAccent,
-        currentIndex: pageIndex,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: [
+          CupertinoIcons.home,
+          CupertinoIcons.shopping_cart,
+          CupertinoIcons.heart,
+          CupertinoIcons.person,
+        ],
+        inactiveColor: Colors.black.withOpacity(0.5),
+        activeColor: Color(0xFFDB3022),
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+        leftCornerRadius: 10,
+        rightCornerRadius: 10,
+        iconSize: 25,
+        elevation: 0,
+        activeIndex: pageIndex,
         onTap: (index) {
           setState(() {
             pageIndex = index;
           });
         },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.lightBlueAccent,
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
       ),
     );
   }
