dfcfaf6a02611fa1e437dddf03c1fc250d246c70
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-03 21:24:54 +0430

    feat: enhance HomeScreen with product display, tabs, and UI improvements

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  List<String> tabs = ['All', 'Category', 'Top', 'Recommonded'];
+  List<String> imageList = [
+    "assets/images/image1.jpg",
+    "assets/images/image2.jpg",
+    "assets/images/image3.jpg",
+    "assets/images/image4.jpg",
+    "assets/images/image5.jpg",
+    "assets/images/image6.jpg",
+  ];
+
+  List<String> productTitles = [
+    "Nike Air Max 270",
+    "Adidas Ultraboost",
+    "Puma RS-X",
+    "Reebok Classic",
+    "New Balance 990v5",
+    "Asics Gel-Kayano 27",
+  ];
+
+  List<String> productPrices = ["\$150", "\$180", "\$120", "\$100", "\$170", "\$160"];
+
+  List<String> productRate = ['4.3', '4.1', '3.6', '4.8', '4.5', '4.3'];
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +41,7 @@ class HomeScreen extends StatelessWidget {
                   Container(
                     padding: EdgeInsets.all(5),
                     height: 50,
-                    width: MediaQuery.of(context).size.width / 1.5,
+                    width: MediaQuery.of(context).size.width / 1.23,
                     decoration: BoxDecoration(
                       color: Colors.black12.withOpacity(0.05),
                       borderRadius: BorderRadius.circular(10),
@@ -40,7 +63,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                   Container(
                     height: 50,
-                    width: MediaQuery.of(context).size.width / 8,
+                    width: MediaQuery.of(context).size.width / 9,
                     decoration: BoxDecoration(
                       color: Colors.black12.withOpacity(0.05),
                       borderRadius: BorderRadius.circular(10),
@@ -58,10 +81,111 @@ class HomeScreen extends StatelessWidget {
               ),
               SizedBox(height: 20),
               Container(
-                height: 150,
+                height: 160,
                 decoration: BoxDecoration(
                   color: Color(0xFFFFF0DD),
                   borderRadius: BorderRadius.all(Radius.circular(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12.withOpacity(0.05),
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Image.asset("assets/images/freed.png"),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: tabs.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return FittedBox(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.black12.withOpacity(0.05),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: FittedBox(
+                            child: Text(tabs[index], style: TextStyle(fontSize: 12)),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 300,
+                child: ListView.builder(
+                  itemCount: imageList.length,
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 200,
+                            child: Stack(
+                              children: [
+                                InkWell(
+                                  onTap: () {},
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.asset(imageList[index], fit: BoxFit.cover),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 10,
+                                  right: 10,
+                                  child: Container(
+                                    width: 30,
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Icon(Icons.favorite_border, size: 16, color: Colors.red),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            productTitles[index],
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Icon(Icons.star, color: Colors.amber, size: 22),
+                              Text('(' + productRate[index] + ')'),
+                              Text(
+                                productPrices[index],
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFDB3022),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
