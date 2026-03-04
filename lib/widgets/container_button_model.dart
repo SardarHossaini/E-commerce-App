35b36a43bfcb7adc505fa7bca57f4b7fcdbee16e
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-04 20:09:01 +0430

    feat: start desing the container component widget

---
+++ lib/widgets/container_button_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ContainerButtonModel extends StatelessWidget {
+  final Color? bgColor;
+  final double? containerWidth;
+  final String itext;
+
+  ContainerButtonModel({super.key, this.bgColor, this.containerWidth, required this.itext});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
