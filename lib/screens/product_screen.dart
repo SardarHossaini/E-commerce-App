9280f7a62ed3c20ecb8b6ebf6ab2b3dca9f1d481
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-04 19:51:45 +0430

    feat: add the rating page and use it

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class ProductScreen extends StatelessWidget {
   ProductScreen({super.key});
@@ -20,22 +21,67 @@ class ProductScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
-            padding: EdgeInsets.all(20),
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(
-                  height: 400,
                   width: MediaQuery.of(context).size.width,
                   child: FanCarouselImageSlider.sliderType1(
                     imagesLink: images,
                     isAssets: true,
                     autoPlay: true,
-                    sliderHeight: 380,
+                    sliderHeight: 350,
+                    displayIndicatorOnSlider: true,
                     showIndicator: true,
+                    expandFitAndZoomable: true,
                   ),
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        Text(
+                          "Warm Zipper",
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 25,
+                          ),
+                        ),
+                        SizedBox(height: 2),
+                        Text(
+                          "Hooded Jacket",
+                          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "\$400.00",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Color(0xFFDB3022),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                RatingBar.builder(
+                  initialRating: 4,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                  itemBuilder: (context, index) => Icon(Icons.star, color: Colors.amber),
+                  onRatingUpdate: (rating) {},
+                ),
+                SizedBox(height: 10),
               ],
             ),
           ),
