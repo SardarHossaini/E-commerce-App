9ded6f321444b8e36eb82c0a479daf11537963a6
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-05 20:28:32 +0430

    feat: last upate of the product details

--- lib/widgets/products_details_popups.dart
+++ lib/widgets/products_details_popups.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 class ProductsDetailsPopups extends StatelessWidget {
   final isStyle = TextStyle(color: Colors.black87, fontWeight: FontWeight.w600, fontSize: 18);
+  List<Color> clrs = [Colors.red, Colors.blue, Colors.green, Colors.amber];
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +14,7 @@ class ProductsDetailsPopups extends StatelessWidget {
           backgroundColor: Colors.transparent,
           context: context,
           builder: (context) => Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
             height: MediaQuery.of(context).size.height / 2.5,
             decoration: BoxDecoration(
               color: Colors.white,
@@ -47,20 +49,77 @@ class ProductsDetailsPopups extends StatelessWidget {
                         children: [
                           Row(
                             children: [
-                              SizedBox(width: 20),
+                              SizedBox(width: 15),
                               Text("S", style: isStyle),
-                              SizedBox(width: 20),
+                              SizedBox(width: 30),
                               Text("M", style: isStyle),
-                              SizedBox(width: 20),
+                              SizedBox(width: 30),
                               Text("L", style: isStyle),
-                              SizedBox(width: 20),
+                              SizedBox(width: 30),
                               Text("XL", style: isStyle),
                             ],
                           ),
                           SizedBox(height: 20),
+                          Container(
+                            child: Row(
+                              children: [
+                                for (var i = 0; i < 4; i++)
+                                  Container(
+                                    margin: EdgeInsets.symmetric(horizontal: 8),
+                                    height: 30,
+                                    width: 30,
+                                    decoration: BoxDecoration(
+                                      color: clrs[i],
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              SizedBox(width: 10),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text("-", style: isStyle),
+                              ),
+                              SizedBox(width: 10),
+                              Text("1"),
+                              SizedBox(width: 10),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text("+", style: isStyle),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ],
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Total Payment", style: isStyle),
+                      Text(
+                        "\$40",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFDB3022),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  InkWell(
+                    onTap: () {},
+                    child: ContainerButtonModel(
+                      itext: "Checkout",
+                      containerWidth: MediaQuery.of(context).size.width,
+                      bgColor: Color(0xFFDB3022),
+                    ),
                   ),
                 ],
               ),
@@ -70,7 +129,7 @@ class ProductsDetailsPopups extends StatelessWidget {
       },
       child: ContainerButtonModel(
         itext: "Buy Now",
-        containerWidth: MediaQuery.of(context).size.width / 1.5,
+        containerWidth: MediaQuery.of(context).size.width / 1.28,
         bgColor: Color(0xFFDB3022),
       ),
     );
