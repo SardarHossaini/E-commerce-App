35c5ccfd71295830dc18ace8ef392669ae29d733
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-05 21:27:33 +0430

    feat: design the cart page

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/widgets/container_button_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
@@ -21,11 +23,128 @@ class CartScreen extends StatelessWidget {
         leading: BackButton(),
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.black,
+        centerTitle: true,
         elevation: 0,
       ),
-      body: Container(
-        color: Colors.white,
-        child: const Center(child: Text('Cart Screen')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Checkbox(
+                            splashRadius: 20,
+                            activeColor: Color(0xFFDB3022),
+                            value: true,
+                            onChanged: (val) {},
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              imagesList[index],
+                              height: 80,
+                              width: 80,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                productTitle[index],
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text("Hooded Jacket", style: TextStyle(color: Colors.black87)),
+                              SizedBox(height: 10),
+                              Text(
+                                prices[index],
+                                style: TextStyle(
+                                  color: Color(0xFFDB3022),
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              TextButton(onPressed: () {}, child: Icon(CupertinoIcons.minus)),
+                              SizedBox(width: 10),
+                              Text(
+                                "1",
+                                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
+                              ),
+                              SizedBox(width: 10),
+                              TextButton(onPressed: () {}, child: Icon(CupertinoIcons.plus)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: productTitle.length,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Select All", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16)),
+                  Checkbox(
+                    splashRadius: 20,
+                    activeColor: Color(0xFFDB3022),
+                    value: false,
+                    onChanged: (val) {},
+                  ),
+                ],
+              ),
+              Divider(height: 20, thickness: 1),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total Payment",
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                  ),
+                  Text(
+                    "\$234",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 18,
+                      color: Color(0xFFDB3022),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                onTap: () {},
+                child: ContainerButtonModel(
+                  itext: "Checkout",
+                  containerWidth: MediaQuery.of(context).size.width,
+                  bgColor: Color(0xFFDB3022),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
